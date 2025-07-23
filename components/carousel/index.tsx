60a3e015a2fbed557cfab78f9509fa11a34ece82
Mateus2201 <mateusvsilva.mv2020@gmail.com>
2025-07-23 01:21:34 -0300

    refatoração do componente Carousel para simplificar o carregamento de imagens e melhorar a legibilidade do código

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -18,45 +18,37 @@ export default function Carousel() {
 
 	useEffect(() => {
 		publicApi.get<CloudinaryImage[]>(`/site-images?folder=registro-site`)
-			.then(({ data }) => {
-				console.log(data);
-				
-
-				setImages(data);
-			})
+			.then(({ data }) => setImages(data))
 			.catch(err => {
 				console.error("Erro ao carregar imagens:", err);
 				setImages([]);
 			});
 	}, []);
 
-	if (images === null) return null; // ainda carregando
+	if (images === null) return null;
 
 	if (images.length === 0) {
-		return (
-			<div className="flex items-center justify-center w-screen h-[60vh] bg-gray-100 text-gray-500 text-xl">
-				Sem imagens disponíveis
-			</div>
-		);
+		return <div className="flex items-center justify-center w-screen h-[60vh] bg-gray-100 text-gray-500 text-xl">
+			Sem imagens disponíveis
+		</div>
 	}
 
-	return (
-		<Swiper
-			modules={[Navigation, Pagination, Scrollbar, A11y]}
-			direction='horizontal'
-			pagination={{ clickable: true }}
-		>
-			{images.map((image, index) => (
-				<SwiperSlide key={image.publicId} className='cursor-pointer'>
-					<Image
-						src={image.url}
-						alt={image.name || `imagem-${index}`}
-						width={1000}
-						height={1000}
-						className='lg:max-h-[60vh] h-auto w-screen object-cover'
-					/>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
+	return <Swiper
+		modules={[Navigation, Pagination, Scrollbar, A11y]}
+		direction='horizontal'
+		pagination={{ clickable: true }}
+	>
+		{images.map((image, index) => (
+			<SwiperSlide key={image.publicId} className='cursor-pointer'>
+				<Image
+					src={image.url}
+					alt={image.name || `imagem-${index}`}
+					width={1400}
+					height={1400}
+					className='w-screen max-h-1/2'
+					priority={index === 0}
+				/>
+			</SwiperSlide>
+		))}
+	</Swiper>
 }
